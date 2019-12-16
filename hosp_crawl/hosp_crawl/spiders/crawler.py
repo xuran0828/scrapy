90711fa51733116a743d253babd588e73ccfa00b
xuran0828 <35069215+xuran0828@users.noreply.github.com>
2019-12-16 21:03:45 +0800

    对页数进行遍历，可能会出现不存在的页数，导致下载失败而停止，所以添加实现5次下载错误才会停止遍历。

--- hosp_crawl/hosp_crawl/spiders/crawler.py
+++ hosp_crawl/hosp_crawl/spiders/crawler.py
@@ -9,7 +9,8 @@ from hosp_crawl.items import *
 class CrawlerSpider(scrapy.Spider):
     name = 'crawler'
     allowed_domains = ['db.yaozh.com']
-
+    max_errors=5
+    num-errors=0
     def start_requests(self):
         for i in range(83221,84880):
             start_urls = 'https://db.yaozh.com/hmap/{}.html'.format(str(i))
@@ -18,28 +19,35 @@ class CrawlerSpider(scrapy.Spider):
             time.sleep(random.random() * 4)
 
     def parse(self, response):
+        
         item = HospCrawlItem()
-        table = response.xpath('//table[@class="table"]/tbody')[0]
-        head = table.xpath('./tr/th/text()').extract()
-        content = table.xpath('./tr/td')
-        content = [''.join(i.xpath('.//text()').extract()).strip() for i in content]
-        a = dict(zip(head, content))
-        d = {
-            'hosp_name':'医院名称',
-            'hosp_alias': '医院别名',
-            'hospital_level': '医院等级',
-            'hosp_type':'医院类型',
-            'founded_date':'建院年份',
-            'operation_way':'经营方式',
-            'website':'医院网址',
-            'phone':'电话',
-            'pr':'省',
-            'city':'市',
-            'addr_detail':'医院地址'
-        }
-        for en, zh in d.items():
-            if zh in a:
-                item[en] = a[zh]
+        if response.text is None:
+            num_errors+=1
+            if num_errors==max_errors:
+                break
+        else:
+            table = response.xpath('//table[@class="table"]/tbody')[0]
+            head = table.xpath('./tr/th/text()').extract()
+            content = table.xpath('./tr/td')
+            content = [''.join(i.xpath('.//text()').extract()).strip() for i in content]
+            a = dict(zip(head, content))
+            d = {
+                'hosp_name':'医院名称',
+                'hosp_alias': '医院别名',
+                'hospital_level': '医院等级',
+                'hosp_type':'医院类型',
+                'founded_date':'建院年份',
+                'operation_way':'经营方式',
+                'website':'医院网址',
+                'phone':'电话',
+                'pr':'省',
+                'city':'市',
+                'addr_detail':'医院地址'
+            }
+            for en, zh in d.items():
+                if zh in a:
+                    item[en] = a[zh]
+            num_errors=0
         return item
 
 
